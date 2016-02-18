aab2fa4c73cf55a954cfd8b22846576737715596
Hugo Josefson <hugo@josefson.org>
2016-02-18 15:51:35 +0100

    Use express for workers too.

--- cluster-worker/index.js
+++ cluster-worker/index.js
@@ -1,18 +1,18 @@
 const cluster = require('cluster');
-const http = require('http');
+const express = require('express');
 
 const j = obj => JSON.stringify(obj, null, 2);
 
 module.exports = () => {
     const log = message => console.log(`${`Worker ${j(cluster.worker.id)}`}: ${message}`);
-    log(`reporting for duty`);
+    log(`Reporting for duty.`);
     process.on('message', message => {
-        log(`incoming message: ${j(message)}`);
+        log(`Incoming message: ${j(message)}`);
     });
+
     // Workers can share any TCP connection
     // In this case it is an HTTP server
-    http.createServer((req, res) => {
-        res.writeHead(200);
-        res.end(`Hello world. This is worker ${cluster.worker.id}.\n`);
-    }).listen(8000);
+    const app = express();
+    app.get('/', (req, res) => res.send(`Hello world. This is worker ${cluster.worker.id}.\n`));
+    app.listen(8000, () => log('App ready at http://localhost:8000/'));
 };
