8edaf06f7c4867ca420c45ca7d2396e50a4a83bf
Hugo Josefson <hugo@josefson.org>
2016-02-18 14:03:40 +0100

    Log received messages in worker.

--- cluster-worker.js
+++ cluster-worker.js
@@ -1,8 +1,14 @@
 const cluster = require('cluster');
 const http = require('http');
 
+const j = obj => JSON.stringify(obj, null, 2);
+
 module.exports = () => {
-    console.log(`worker ${JSON.stringify(cluster.worker.id, null, 2)} reporting for duty`);
+    const log = message => console.log(`${`Worker ${j(cluster.worker.id)}`}: ${message}`);
+    log(`reporting for duty`);
+    process.on('message', message => {
+        log(`incoming message: ${j(message)}`);
+    });
     // Workers can share any TCP connection
     // In this case it is an HTTP server
     http.createServer((req, res) => {
